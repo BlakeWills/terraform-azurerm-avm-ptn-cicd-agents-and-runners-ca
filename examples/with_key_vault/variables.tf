3a5bb4ac3472c0da1a262804d847a1dcb289914b
Blake <blakewills@outlook.com>
2024-04-03 14:35:44 +0100

    run pre-commit

--- examples/with_key_vault/variables.tf
+++ examples/with_key_vault/variables.tf
@@ -1,3 +1,20 @@
+variable "ado_organization_url" {
+  type        = string
+  description = "Azure DevOps Organisation URL"
+}
+
+variable "personal_access_token" {
+  type        = string
+  description = "The personal access token used for agent authentication to Azure DevOps."
+  sensitive   = true
+}
+
+variable "container_image_name" {
+  type        = string
+  default     = "azure-pipelines:latest"
+  description = "Name of the container image to build and push to the container registry"
+}
+
 # tflint-ignore: terraform_variable_separate, terraform_standard_module_structure
 variable "enable_telemetry" {
   type        = bool
@@ -7,21 +24,4 @@ This variable controls whether or not telemetry is enabled for the module.
 For more information see <https://aka.ms/avm/telemetryinfo>.
 If it is set to false, then no telemetry will be collected.
 DESCRIPTION
-}
-
-variable "personal_access_token" {
-  type        = string
-  sensitive   = true
-  description = "The personal access token used for agent authentication to Azure DevOps."
-}
-
-variable "container_image_name" {
-  type        = string
-  description = "Name of the container image to build and push to the container registry"
-  default     = "azure-pipelines:latest"
-}
-
-variable "ado_organization_url" {
-  type        = string
-  description = "Azure DevOps Organisation URL"
 }
--- locals.tf
+++ locals.tf
@@ -3,6 +3,10 @@ locals {
 }
 
 locals {
+  container_registry_user_assigned_identity = var.container_registry_user_assigned_identity != null ? var.container_registry_user_assigned_identity : local.single_id
+  # Use the explicit identity for each operation, or, use the SINGLE supplied identity. Fail if multiple, or no identities are supplied.
+  has_single_id                    = (var.managed_identities.system_assigned != (length(var.managed_identities.user_assigned_resource_ids) == 1))
+  key_vault_user_assigned_identity = var.key_vault_user_assigned_identity != null ? var.key_vault_user_assigned_identity : local.single_id
   managed_identities = {
     system_assigned_user_assigned = (var.managed_identities.system_assigned || length(var.managed_identities.user_assigned_resource_ids) > 0) ? {
       this = {
@@ -11,11 +15,5 @@ locals {
       }
     } : {}
   }
-
-  # Use the explicit identity for each operation, or, use the SINGLE supplied identity. Fail if multiple, or no identities are supplied.
-  has_single_id = (var.managed_identities.system_assigned != (length(var.managed_identities.user_assigned_resource_ids) == 1))
-  single_id     = local.has_single_id ? var.managed_identities.system_assigned ? "System" : tolist(var.managed_identities.user_assigned_resource_ids)[0] : null
-
-  key_vault_user_assigned_identity          = var.key_vault_user_assigned_identity != null ? var.key_vault_user_assigned_identity : local.single_id
-  container_registry_user_assigned_identity = var.container_registry_user_assigned_identity != null ? var.container_registry_user_assigned_identity : local.single_id
+  single_id = local.has_single_id ? var.managed_identities.system_assigned ? "System" : tolist(var.managed_identities.user_assigned_resource_ids)[0] : null
 }
